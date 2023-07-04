2956f03675d9cef23819fbb9b4ee0907948cf014
KirVelikiy <lisov.kirill50@gmail.com>
2023-07-04 13:02:40 +0400

    Some applications

---
+++ src/Gallary.js
@@ -0,0 +1,40 @@
+import { sculptureList } from "./data";
+import { useState } from "react";
+
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+
+    const handleClick = () => {
+        setIndex(index + 1);
+    }
+
+    const handleMoreClick = () => {
+        setShowMore(!showMore);
+    }
+
+    let sculpture = sculptureList[index]
+
+    return (
+        <>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name}</i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
+    );
+}
