8c4d6dfe733ba047095e2347f47f9d9c8f6114eb
KirVelikiy <lisov.kirill50@gmail.com>
2023-07-04 13:30:50 +0400

    EditProfile app

--- src/App.js
+++ src/App.js
@@ -1,76 +1,44 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-let nextIndex = 0
+export default function EditProfile() {
+    const [firstName, setFirstName] = useState('Jane');
+    const [lastName, setLastName] = useState('Jacob');
+    const [isEdit, setIsEdit] = useState(false);
 
-export default function List() {
-    const [list, setList] = useState([])
-    const [name, setName] = useState('')
-    const [insertIndex, setInsertIndex] = useState(0)
-
-    function getCopyList(l) {
-        return [...l]
-    }
-    function handleAddClick() {
-        const nextList = [
-            ...list.slice(0, insertIndex),
-            {id: nextIndex++, name: name},
-            ...list.slice(insertIndex)
-        ];
-        setList(nextList);
-        setName('');
-    }
-
-    function handleReverseClick(e) {
-        const nextList = getCopyList(list);
-        nextList.reverse();
-        setList(nextList);
-        e.stopPropagation()
-    }
-
-    function handleSortClick() {
-        const nextList = getCopyList(list);
-        nextList.sort((a, b) => (a.name > b.name) ? 1 : -1);
-        setList(nextList);
+    function handleSubmitClick(e) {
+        e.preventDefault();
+        setIsEdit(!isEdit);
     }
 
     return (
-        <>
-            <button onClick={handleReverseClick}>
-                reverse
+        <form>
+            <label>
+                First name:{' '}
+                {isEdit ?
+                    <input
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                    /> :
+                    <b>{firstName}</b>
+                }
+            </label>
+            <label>
+                Last name:{' '}
+                {isEdit ?
+                    <input
+                        value={lastName}
+                        onChange={e => setLastName(e.target.value)}
+                    /> :
+                    <b>{lastName}</b>
+                }
+            </label>
+            <button type="submit" onClick={handleSubmitClick}>
+                {isEdit ?
+                    "Save Profile" :
+                    "Edit Profile"
+                }
             </button>
-            <button onClick={handleSortClick}>
-                sort
-            </button>
-            <div>
-                <h2>Insert Index value:</h2>
-                <input
-                    readOnly={true}
-                    value={insertIndex}
-                />
-                <button
-                    onClick={() => setInsertIndex(insertIndex + 1)} >
-                    +1
-                </button>
-                <button
-                    onClick={() => setInsertIndex(insertIndex - 1)} >
-                    -1
-                </button>
-            </div>
-            <h1>List values:</h1>
-            <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button onClick={handleAddClick}>
-                Add
-            </button>
-            <ul>
-                {list.map((element) => (
-                    <li key={element.id}>
-                        {element.name}
-                    </li>
-                ))}
-            </ul>
-        </>
+            <p><i>Hello, {firstName} {lastName}!</i></p>
+        </form>
     );
 }
