438655d97238ad2c09e43778d446c4d0656849e4
KirVelikiy <lisov.kirill50@gmail.com>
2023-07-07 19:06:08 +0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { useRef, forwardRef, useImperativeHandle } from "react";
+import { useState, useRef, useEffect } from 'react';
 
-const MyInput = forwardRef((props, ref) => {
-    const realInputRef = useRef(null);
-    useImperativeHandle(ref, () => ({
-        focus() {
-            realInputRef.current.focus();
-        },
-    }));
-    return <input {...props} ref={realInputRef} />;
-});
+function VideoPlayer({ src, isPlaying }) {
+    const ref = useRef(null);
 
-export default function Form() {
-    const inputRef = useRef(null);
+    useEffect(() => {
+        if (isPlaying) {
+            ref.current.play();
+        } else {
+            ref.current.pause();
+        }
+    });
 
-    function handleClick() {
-        inputRef.current.focus();
-    }
+    return <video ref={ref} src={src} loop playsInline />;
+}
 
+export default function App() {
+    const [isPlaying, setIsPlaying] = useState(false);
     return (
         <>
-            <MyInput ref={inputRef} />
-            <button onClick={handleClick}>
-                Focus the input
+            <button onClick={() => setIsPlaying(!isPlaying)}>
+                {isPlaying ? 'Pause' : 'Play'}
             </button>
+            <VideoPlayer
+                isPlaying={isPlaying}
+                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+            />
         </>
     );
 }
